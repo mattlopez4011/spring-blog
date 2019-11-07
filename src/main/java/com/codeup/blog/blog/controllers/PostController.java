0efeaf168275e1e32f899db01e0da7f6541cbc23
Matt Lopez <matthew.lopez4011@gmail.com>
2019-11-07 14:40:51 -0600

    notes

--- src/main/java/com/codeup/blog/blog/controllers/PostController.java
+++ src/main/java/com/codeup/blog/blog/controllers/PostController.java
@@ -88,3 +88,19 @@ public class PostController {
     }
 
 }
+
+
+//==============
+// Exercise instructions
+//One-to-Many Mini Exercise:
+//        You will create a one-to-many relationship between the Post entity and the PostImage entity. Consider that...
+//        - "One post may have many images."
+//        - "Many images belong to one post."
+//        1. In the relationship-practice branch of your Spring Blog's, add an entity of PostImage with the following fields:
+//        id (typical primary key annotations)
+//        image_title VARCHAR(100) NOT NULL
+//        url VARCHAR(1000) NOT NULL - the url is for simple hot linking of images for now
+//        post_id (a FK that will be created with the proper One-To-Many mapping)
+//        2. Seed your posts table with a blog post about cats
+//        3. Seed your post_images table with three images related to cats (use a hot-linked address for now).
+//        4. Create a simple view that includes displays the title of the cat blog post the three images
